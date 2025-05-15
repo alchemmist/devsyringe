0e66934230539beb88c925b8dea5c44711b2530b
Alchemmist <anton.ingrish@gmail.com>
2025-05-15 20:51:34 +0300

    deserialzie yaml config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+mod config_model;
+
+use config_model::Config;
+use std::fs::File;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let file = File::open("./injection.yaml")?;
+    let config: Config = serde_yaml::from_reader(file)?;
+    println!("{:#?}", config.serums["https_host"]);
+    Ok(())
 }
